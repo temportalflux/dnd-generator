3639a362069f22c68b14dad81825e5faaa88fe6f
temportalflux <dustin.yost.t@gmail.com>
2019-09-18 08:04:59 -0400

    Add display of npc json data

--- src/components/DisplayNpc.jsx
+++ src/components/DisplayNpc.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
 
-export default class FilterMenu extends React.Component
+export default class DisplayNpc extends React.Component
 {
-	render() {
-    return (
-      <Segment>
-				TODO Display
-      </Segment>
-    );
-  }
+	render()
+	{
+		const { data } = this.props;
+		return (
+			<Segment>
+				<pre>{JSON.stringify(data, null, 2)}</pre>
+			</Segment>
+		);
+	}
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import {Grid} from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import FilterMenu from '../components/FilterMenu';
 import DisplayNpc from '../components/DisplayNpc';
-import {generate} from '../generator/index';
+import { generate } from '../generator/index';
 
 export default class Home extends React.Component
 {
@@ -11,24 +11,33 @@ export default class Home extends React.Component
 	{
 		super(props);
 		this.generate = this.generate.bind(this);
+		this.state = {
+			npc: undefined
+		};
 	}
 
 	generate(filter)
 	{
-		console.log(generate(filter));
+		this.setState({
+			npc: generate(filter)
+		});
 	}
 
 	render()
 	{
 		return (
-			<Grid columns={1}>
+			<Grid columns={2}>
 				<Grid.Row>
 					<Grid.Column>
 						<FilterMenu
 							generate={this.generate}
 						/>
 					</Grid.Column>
-						<DisplayNpc/>
+					<Grid.Column>
+						<DisplayNpc
+							data={this.state.npc}
+						/>
+					</Grid.Column>
 				</Grid.Row>
 			</Grid>
 		);
