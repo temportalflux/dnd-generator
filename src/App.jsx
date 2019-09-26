96ba5982db004dbb9a03463133de5c2296b7ead0
temportalflux <dustin.yost.t@gmail.com>
2019-09-25 21:13:55 -0400

    Move data files to a root directory

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,41 @@ import React from 'react';
 import { Container } from "semantic-ui-react";
 import Home from './pages/index';
 
+async function performGitRequest(endpoint)
+{
+	return await fetch(`https://api.github.com${endpoint}`, {
+		method: 'GET',
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/vnd.github.v3+json'
+		},
+		redirect: 'follow',
+		referrer: 'no-referrer'
+	});
+}
+
 class App extends React.Component
 {
+
+	async componentDidMount()
+	{
+		// Can fetch all data here
+
+		// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+		// https://developer.github.com/v3/#current-version
+		const resultCommits = await performGitRequest('/repos/temportalflux/dnd-generator/commits');
+		const commits = await resultCommits.json();
+		console.log(commits);
+
+		const resultLatestTree = await performGitRequest(`/repos/temportalflux/dnd-generator/git/trees/${commits[0].commit.tree.sha}`);
+		const tree = await resultLatestTree.json();
+		console.log(tree);
+		
+
+	}
+
 	render()
 	{
 		return (
@@ -12,6 +45,7 @@ class App extends React.Component
 			</Container>
 		);
 	}
+
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,104 +1,13 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react';
-import FilterMenu from '../components/FilterMenu';
-import DisplayNpc from '../components/DisplayNpc';
-import { generate } from '../generator/index';
-import NpcDataTree from '../components/NpcDataTree';
-import lodash from 'lodash';
-import { Generator, GenerationEntry } from '../generator/Generator';
+//import { Grid } from 'semantic-ui-react';
+//import FilterMenu from '../components/FilterMenu';
+//import DisplayNpc from '../components/DisplayNpc';
+//import { generate } from '../generator/index';
+//import NpcDataTree from '../components/NpcDataTree';
+//import lodash from 'lodash';
+//import { Generator, GenerationEntry } from '../generator/Generator';
 
-const { getTable } = require('../Data');
-
-const TEMP_NPC_DATA = {
-	"meta": {
-		"stats": {
-			"abilityScores": {
-				"stringify": "STR($(strength)) DEX($(dexterity)) CON($(constitution)) INT($(intelligence)) WIS($(wisdom)) CHA($(charisma))"
-			},
-			"alignmentTendancies": {
-				"stringify": "$(ethical.positive)/$(ethical.neutral)/$(ethical.negative) - $(moral.positive)/$(moral.neutral)/$(moral.negative)",
-				"ethical": {
-					"stringify": "$(positive)/$(neutral)/$(negative)"
-				},
-				"moral": {
-					"stringify": "$(positive)/$(neutral)/$(negative)"
-				}
-			}
-		},
-		"identity": {
-			"name": {},
-			"pronouns": {
-				"stringify": "$(singular)/$(thirdPerson)/$(possessive)",
-				"singular": {
-					"canReroll": false
-				},
-				"thirdPerson": {
-					"canReroll": false
-				},
-				"possessive": {
-					"canReroll": false
-				}
-			}
-		},
-		"description": {
-			"face": {
-				"attractiveness": {}
-			},
-			"hair": {},
-			"beard": {}
-		},
-		"personality": {}
-	},
-	"values": {
-		"stats": {
-			"abilityScores": {
-				"strength": 9, "dexterity": 9, "constitution": 9, "intelligence": 9, "wisdom": 9, "charisma": 9
-			},
-			"alignmentTendancies": {
-				"ethical": { "positive": 5, "neutral": 2, "negative": 1 },
-				"moral": { "positive": 3, "neutral": 9, "negative": 3 }
-			}
-		},
-		"identity": {
-			"name": ["Jim", "Bob", "the third"],
-			"pronouns": { "singular": "they", "thirdPerson": "them", "possessive": "their" },
-			"genderIdentity": "man",
-			"genderExpression": "nonbinary",
-			"sex": "male",
-			"sexualOrientation": "straight"
-		},
-		"description": {
-			"age": 34,
-			"height": 176,
-			"weight": 184,
-			"race": "human",
-			"subrace": undefined,
-			"skin": "white",
-			"eyeColor": "green",
-			"face": {
-				"shape": "triangular",
-				"attractiveness": {
-					"modifier": "incredibly",
-					"quality": "common"
-				}
-			},
-			"hair": {
-				"type": "normal",
-				"length": "cropped",
-				"style": "wavy",
-				"color": "dyed blue"
-			},
-			"beard": {
-				"length": "gigantic, braided",
-				"shape": "squared beard"
-			}
-		},
-		"personality": {
-			"alignmentMoral": "good",
-			"deity": "Tyr, God of justice (Lawful Good)"
-		}
-	}
-};
+//const { getTable } = require('../Data');
 
 export default class Home extends React.Component
 {
@@ -106,9 +15,10 @@ export default class Home extends React.Component
 	constructor(props)
 	{
 		super(props);
-		this.generate = this.generate.bind(this);
-		this.onRerollClicked = this.onRerollClicked.bind(this);
+		//this.generate = this.generate.bind(this);
+		//this.onRerollClicked = this.onRerollClicked.bind(this);
 
+		/*
 		const npc = getTable('npc');
 		const generator = new Generator();
 
@@ -120,86 +30,13 @@ export default class Home extends React.Component
 		generator.setGenerationOrder(npc.generationOrder || []);
 		generator.generate();
 
-		
-		/*
-		const data = [
-			{
-				category: 'identity',
-				key: 'sex',
-				source: '{roll:identity/sex}'
-			},
-			{
-				category: 'identity',
-				key: 'genderIdentity',
-				source: '{roll:identity/genderIdentity}',
-			},
-			{
-				category: 'stats',
-				key: 'alignmentTendancies',
-				stringify: "$(ethical.positive)/$(ethical.neutral)/$(ethical.negative) - $(moral.positive)/$(moral.neutral)/$(moral.negative)",
-				children: [
-					{
-						key: 'ethical',
-						stringify: "$(positive)/$(neutral)/$(negative)",
-						dependencies: [
-							'identity.sex'
-						],
-						children: [
-							{
-								key: 'positive',
-								name: 'Lawful',
-								source: "{roll:traits/alignment/tendancy}",
-							},
-							{
-								key: 'neutral',
-								name: 'Neutral',
-								source: "{roll:traits/alignment/tendancy}",
-							},
-							{
-								key: 'negative',
-								name: 'Chaotic',
-								source: "{roll:traits/alignment/tendancy}",
-							}
-						]
-					},
-					{
-						key: 'moral',
-						stringify: "$(positive)/$(neutral)/$(negative)",
-						children: [
-							{
-								key: 'positive',
-								name: 'Good',
-								source: "{roll:traits/alignment/tendancy}",
-							},
-							{
-								key: 'neutral',
-								name: 'Neutral',
-								source: "{roll:traits/alignment/tendancy}",
-							},
-							{
-								key: 'negative',
-								name: 'Evil',
-								source: "{roll:traits/alignment/tendancy}",
-							}
-						]
-					}
-				]
-			},
-		];
-
-		data.forEach((entryData) => {
-			generator.addEntry(new GenerationEntry(lodash.assign({}, entryData, {generator: generator})));
-		})
-
-		console.log(generator);
-		//*/
-
 		this.state = {
-			npc: TEMP_NPC_DATA,
 			generator: generator
 		};
+		//*/
 	}
 
+	/*
 	generate(filter)
 	{
 		this.setState({
@@ -220,6 +57,7 @@ export default class Home extends React.Component
 		}
 		this.setState({ generator: generator });
 	}
+	//*/
 
 	render()
 	{
@@ -239,16 +77,16 @@ export default class Home extends React.Component
 								data={this.state.npc}
 							/>
 						</Grid.Column>
-		*/
-		return (
-			<div>
 
 				<NpcDataTree
 					data={this.state.npc}
 					generator={this.state.generator}
 					onRerollClicked={this.onRerollClicked}
 				/>
-
+		*/
+		return (
+			<div>
+				There is nothing here right now
 			</div>
 		);
 	}
