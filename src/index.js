c0e93739a14c9347e475973d448d660c679beec7
temportalflux <dustin.yost.t@gmail.com>
2019-11-24 22:38:13 -0500

    Browser weirdness

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ console.log('Versions:', {
 	react: React.version,
 	node: process.versions.node,
 });
+console.log(`Serving at public url: ${process.env.PUBLIC_URL}`);
 
 // basename is required because the apge is being served under github pages (aliased to a domain site).
+// TODO: Maybe switch out of browser router? https://blog.logrocket.com/how-react-hooks-can-replace-react-router/
 ReactDOM.render((
-	<BrowserRouter basename="/dnd-generator">
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<App />
 	</BrowserRouter>
 ), document.getElementById('root'));
