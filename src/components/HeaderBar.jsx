61c66fcde3527c0e35ccf21131c49a3b4187759a
temportalflux <dustin.yost.t@gmail.com>
2020-01-25 09:04:47 -0500

    Add header label with version number
    Closes issue #27

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Menu, Button, Icon } from 'semantic-ui-react';
+import { Menu, Button, Icon, Header } from 'semantic-ui-react';
 import { HeaderBarLink } from './HeaderBarLink';
 import TableCollection from '../storage/TableCollection';
+import packageJson from '../../package.json';
 
 export class HeaderBar extends React.Component
 {
@@ -19,6 +20,11 @@ export class HeaderBar extends React.Component
 				<HeaderBarLink path={'home'}>Home</HeaderBarLink>
 				<HeaderBarLink path={'npc'}>NPC</HeaderBarLink>
 				<HeaderBarLink path={'data'}>Data</HeaderBarLink>
+
+				<Header style={{width: '100%', textAlign: 'center'}}>
+					D&D Generator
+					<Header.Subheader>{packageJson.versionPrefix} v{packageJson.version}</Header.Subheader>
+				</Header>
 
 				<Menu.Item position='right'>
 					<Button color='red' onClick={this.onClearStorage.bind(this)}>
